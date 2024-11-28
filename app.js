e6b7897ebfa74507dac64a1ddf584d77e9a0605f
Apalakimmortal <apalakimmortal045@gmail.com>
2024-11-28 20:09:31 +0530

    Rbac_implemented

---
+++ app.js
@@ -0,0 +1,149 @@
+const express = require('express');
+const createHttpError = require('http-errors');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const session = require('express-session');
+const connectFlash = require('connect-flash');
+const passport = require('passport');
+const connectMongo = require('connect-mongo');
+const { ensureLoggedIn } = require('connect-ensure-login');
+const { roles } = require('./utils/constants');
+
+// Initialization
+const app = express();
+app.use(morgan('dev'));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const MongoStore = new connectMongo(session);
+// Init Session
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      // secure: true,
+      httpOnly: true,
+    },
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
+
+// For Passport JS Authentication
+app.use(passport.initialize());
+app.use(passport.session());
+require('./utils/passport.auth');
+
+app.use((req, res, next) => {
+  res.locals.user = req.user;
+  next();
+});
+
+// Connect Flash
+app.use(connectFlash());
+app.use((req, res, next) => {
+  res.locals.messages = req.flash();
+  next();
+});
+
+// Routes
+app.use('/', require('./routes/index.route'));
+app.use('/auth', require('./routes/auth.route'));
+app.use(
+  '/user',
+  ensureLoggedIn({ redirectTo: '/auth/login' }),
+  require('./routes/user.route')
+);
+app.use(
+  '/admin',
+  ensureLoggedIn({ redirectTo: '/auth/login' }),
+  ensureAdmin,
+  require('./routes/admin.route')
+);
+
+// 404 Handler
+app.use((req, res, next) => {
+  next(createHttpError.NotFound());
+});
+
+// Error Handler
+app.use((error, req, res, next) => {
+  error.status = error.status || 500;
+  res.status(error.status);
+  res.render('error_40x', { error });
+});
+
+// Setting the PORT
+const PORT = process.env.PORT || 3000;
+
+// Making a connection to MongoDB
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    dbName: process.env.DB_NAME,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('💾 MongoDB connected...');
+
+    const MongoStore = connectMongo.create({
+      client: mongoose.connection.getClient(), // Use the native MongoDB client
+    });
+
+    app.use(
+      session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+          httpOnly: true,
+          // secure: true, // Uncomment if using HTTPS
+        },
+        store: MongoStore,
+      })
+    );
+
+    startServer();
+  })
+  .catch((err) => {
+    console.error('MongoDB connection error:', err.message);
+    process.exit(1); // Exit on connection error
+  });
+
+function startServer() {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running at http://localhost:${PORT}`);
+  });
+}
+
+
+// function ensureAuthenticated(req, res, next) {
+//   if (req.isAuthenticated()) {
+//     next();
+//   } else {
+//     res.redirect('/auth/login');
+//   }
+// }
+
+function ensureAdmin(req, res, next) {
+  if (req.user.role === roles.admin) {
+    next();
+  } else {
+    req.flash('warning', 'you are not Authorized to see this route');
+    res.redirect('/');
+  }
+}
+
+function ensureModerator(req, res, next) {
+  if (req.user.role === roles.moderator) {
+    next();
+  } else {
+    req.flash('warning', 'you are not Authorized to see this route');
+    res.redirect('/');
+  }
+}
---
+++ routes/auth.route.js
@@ -0,0 +1,186 @@
+const router = require('express').Router();
+const User = require('../models/user.model');
+const { body, validationResult } = require('express-validator');
+const passport = require('passport');
+const { ensureLoggedOut, ensureLoggedIn } = require('connect-ensure-login');
+const { registerValidator } = require('../utils/validators');
+
+router.get(
+  '/login',
+  ensureLoggedOut({ redirectTo: '/' }),
+  async (req, res, next) => {
+    res.render('login');
+  }
+);
+
+router.post(
+  '/login',
+  ensureLoggedOut({ redirectTo: '/' }),
+  passport.authenticate('local', {
+    // successRedirect: '/',
+    successReturnToOrRedirect: '/',
+    failureRedirect: '/auth/login',
+    failureFlash: true,
+  })
+);
+
+router.get(
+  '/register',
+  ensureLoggedOut({ redirectTo: '/' }),
+  async (req, res, next) => {
+    res.render('register');
+  }
+);
+
+router.post(
+  '/register',
+  ensureLoggedOut({ redirectTo: '/' }),
+  registerValidator,
+  async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        errors.array().forEach((error) => {
+          req.flash('error', error.msg);
+        });
+        res.render('register', {
+          email: req.body.email,
+          messages: req.flash(),
+        });
+        return;
+      }
+
+      const { email } = req.body;
+      const doesExist = await User.findOne({ email });
+      if (doesExist) {
+        req.flash('warning', 'Username/email already exists');
+        res.redirect('/auth/register');
+        return;
+      }
+      const user = new User(req.body);
+      await user.save();
+      req.flash(
+        'success',
+        `${user.email} registered succesfully, you can now login`
+      );
+      res.redirect('/auth/login');
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.get(
+  '/logout',
+  ensureLoggedIn({ redirectTo: '/' }),
+  async (req, res, next) => {
+    // Logout with a callback
+    req.logout(function (err) {
+      if (err) {
+        return next(err);  // Pass any error to the next middleware
+      }
+      res.redirect('/');  // Redirect to the homepage after successful logout
+    });
+  }
+);
+
+
+module.exports = router;
+
+// function ensureAuthenticated(req, res, next) {
+//   if (req.isAuthenticated()) {
+//     next();
+//   } else {
+//     res.redirect('/auth/login');
+//   }
+// }
+
+// function ensureNOTAuthenticated(req, res, next) {
+//   if (req.isAuthenticated()) {
+//     res.redirect('back');
+//   } else {
+//     next();
+//   }
+// }
+
+// const router = require('express').Router();
+// const User = require('../models/user.model');
+// const { body, validationResult } = require('express-validator');
+// const passport = require('passport');
+// const { ensureLoggedOut, ensureLoggedIn } = require('connect-ensure-login');
+// const { registerValidator } = require('../utils/validators');
+
+// router.get(
+//   '/login',
+//   ensureLoggedOut({ redirectTo: '/' }),
+//   async (req, res, next) => {
+//     res.render('login');
+//   }
+// );
+
+// router.post(
+//   '/login',
+//   ensureLoggedOut({ redirectTo: '/' }),
+//   passport.authenticate('local', {
+//     successReturnToOrRedirect: '/',
+//     failureRedirect: '/auth/login',
+//     failureFlash: true,
+//   })
+// );
+
+// router.get(
+//   '/register',
+//   ensureLoggedOut({ redirectTo: '/' }),
+//   async (req, res, next) => {
+//     res.render('register');
+//   }
+// );
+
+// router.post(
+//   '/register',
+//   ensureLoggedOut({ redirectTo: '/' }),
+//   registerValidator,
+//   async (req, res, next) => {
+//     try {
+//       const errors = validationResult(req);
+//       if (!errors.isEmpty()) {
+//         errors.array().forEach((error) => {
+//           req.flash('error', error.msg);
+//         });
+//         res.render('register', {
+//           email: req.body.email,
+//           messages: req.flash(),
+//         });
+//         return;
+//       }
+
+//       const { email } = req.body;
+//       const doesExist = await User.findOne({ email });
+//       if (doesExist) {
+//         req.flash('warning', 'Username/email already exists');
+//         res.redirect('/auth/register');
+//         return;
+//       }
+//       const user = new User(req.body);
+//       await user.save();
+//       req.flash(
+//         'success',
+//         `${user.email} registered successfully, you can now login`
+//       );
+//       res.redirect('/auth/login');
+//     } catch (error) {
+//       next(error);
+//     }
+//   }
+// );
+
+// router.get(
+//   '/logout',
+//   ensureLoggedIn({ redirectTo: '/' }),
+//   async (req, res, next) => {
+//     req.logout();
+//     res.redirect('/');
+//   }
+// );
+
+// module.exports = router;
